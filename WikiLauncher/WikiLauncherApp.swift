d8bc84ec4379c40779ac8263243ec1dc0777e411
Danil Mironov <danmir13@yandex-team.ru>
2024-09-09 21:57:47 +0200

    Initial Commit

---
+++ WikiLauncher/WikiLauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiLauncherApp.swift
+//  WikiLauncher
+//
+//  Created by Danil Mironov on 09.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct WikiLauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
